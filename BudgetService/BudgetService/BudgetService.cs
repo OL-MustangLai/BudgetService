c8b4b1710cd1916acff162f88afb5b5ec34682aa
Armin <armin.chuang@nextfortune.tw>
2024-12-25 18:20:54 +0800

    good

--- BudgetService/BudgetService/BudgetService.cs
+++ BudgetService/BudgetService/BudgetService.cs
@@ -1,28 +1,46 @@
+using System.Runtime.Intrinsics.X86;
+using NSubstitute.Core;
+
 namespace BudgetService.BudgetService;
 
 public class BudgetService(IBudgetRepo repo)
 {
-    public int Query(DateTime start, DateTime end)
+    public decimal Query(DateTime start, DateTime end)
     {
         var totalDays = (end - start).Days + 1;
-        
-        if (totalDays <1)
+
+        if (totalDays < 1)
         {
             return 0;
         }
-        
-        var budgets = repo.GetAll();
 
-        var daysInMonth = CalculateDays(start);
+        var budgetsList = repo.GetAll();
+        var budgetsLookup = budgetsList.ToDictionary(x => x.YearMonth, x => x.Amount);
 
-        var singleOrDefault = budgets.SingleOrDefault(x=>x.YearMonth == start.ToString("yyyyMM"));
+        var firstMonthBudget = BudgetPerDay(start, budgetsLookup) * (CalculateDays(start) - start.Day+1);
 
-        return (int)(singleOrDefault.Amount / daysInMonth * totalDays);
-        
+        var EndMonthBudget = BudgetPerDay(end, budgetsLookup) * end.Day;
+
+        return (firstMonthBudget + EndMonthBudget + GetBudgetPerMonth(budgetsList,start,end));
+    }
+
+    private decimal GetBudgetPerMonth(List<Budget> budgets, DateTime start, DateTime end)
+    {
+        var startDate =int.Parse(start.Year.ToString()+start.Month.ToString());
+        var endDate =int.Parse(end.Year.ToString()+end.Month.ToString());
+
+        var sum = budgets.Where(x=>startDate<int.Parse(x.YearMonth)&&int.Parse(x.YearMonth)<endDate).Sum(x=>x.Amount);
+        return sum;
+    }
+
+    private static decimal BudgetPerDay(DateTime dateTime, Dictionary<string, decimal> lookup)
+    {
+        var budgetPerDay = lookup[dateTime.ToString("yyyyMM")];
+        return budgetPerDay / CalculateDays(dateTime);
     }
 
     private static int CalculateDays(DateTime start)
     {
-        return DateTime.DaysInMonth(start.Year,start.Month);
+        return DateTime.DaysInMonth(start.Year, start.Month);
     }
 }
--- BudgetService/BudgetService/UnitTest1.cs
+++ BudgetService/BudgetService/UnitTest1.cs
@@ -67,4 +67,64 @@ public class Tests
 
         query.Should().Be(0);
     }
+    
+    [Test]
+    public void Query_Days_Cross_Month()
+    {
+        
+        _budgetRepo.GetAll().Returns(
+        [
+            new Budget
+            {
+                YearMonth = "202411",
+                Amount = 30000
+            },
+
+            new Budget
+            {
+                YearMonth = "202412",
+                Amount = 62000
+            }
+
+        ]);
+        var start = new DateTime(2024, 11, 25);
+        var end = new DateTime(2024, 12, 3);
+
+        var query = _budgetService.Query(start, end);
+
+        query.Should().Be(12000);
+    }
+    
+    [Test]
+    public void Query_Days_Cross_Over_Two_Month()
+    {
+        
+        _budgetRepo.GetAll().Returns(
+        [
+            
+            new Budget
+            {
+                YearMonth = "202410",
+                Amount = 31000
+            },
+            new Budget
+            {
+                YearMonth = "202411",
+                Amount = 30000
+            },
+
+            new Budget
+            {
+                YearMonth = "202412",
+                Amount = 62000
+            }
+
+        ]);
+        var start = new DateTime(2024, 10, 25);
+        var end = new DateTime(2024, 12, 3);
+
+        var query = _budgetService.Query(start, end);
+
+        query.Should().Be(43000);
+    }
 }
